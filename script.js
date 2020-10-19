9dde6ed7249f6072ccb7be5fa95f14569aa67f69
ReQss <46198811+ReQss@users.noreply.github.com>
2020-10-19 22:15:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 prompt("Sprawdz odcinek zanim przystapisz do ogladania FUNgineer ");
 prompt("Widnieje on w lewym gornym rogu na streamie, nie pomyl sie ");
-prompt("Lista znajduje sie w prawym rogu z napisem "Odcinki" ");
+prompt("Lista znajduje sie w prawym rogu z napisem - Odcinki ");
