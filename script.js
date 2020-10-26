7d3cf8f1dc41823f2d16bbff32f1a4a16f431609
ReQss <46198811+ReQss@users.noreply.github.com>
2020-10-26 21:01:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+#Miejsce dodawania i zmiany odcinków
 var lista = [
   'https://ebd.cda.pl/620x368/367290174',
   'https://ebd.cda.pl/620x368/367292850'
@@ -6,23 +7,20 @@ function podmiana(odcinek){
 document.getElementById("animo").src= lista[odcinek];
 
 }
-
-
 function przypinka(){
-	lista1 = document.getElementById("lista").style.opacity;
+	lista_strona = document.getElementById("lista").style.display;
 	
-	if (lista1 == '0'){
-        document.getElementById("lista").style.opacity = '1';
+	if (lista_strona == 'none'){
+        document.getElementById("lista").style.display = 'block'
     	}
     else{
-        document.getElementById("lista").style.opacity = '0';
+        document.getElementById("lista").style.display = 'none';
     	}
-	stream = document.getElementById("streamokno").style.opacity;
-	if (stream == '0'){
-        document.getElementById("streamokno").style.opacity = '1'
+	stream = document.getElementById("streamokno").style.display;
+	if (stream == 'none'){
+        document.getElementById("streamokno").style.display = 'block'
     	}
     else{
-        document.getElementById("streamokno").style.opacity = '0';
+        document.getElementById("streamokno").style.display = 'none';
     	}
 }
-
