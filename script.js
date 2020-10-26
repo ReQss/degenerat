f160e44a59ac991479faa1e59e07380bcf96d0b3
ReQss <46198811+ReQss@users.noreply.github.com>
2020-10-26 20:53:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,16 +21,6 @@ function przypinka(){
         document.getElementById("streamokno").style.opacity = '0';
     	}
 }
-streamtitle = document.getElementsByClassName("tw-c-text-overlay tw-font-size-5");
-element = document.querySelector("stream-info-card-component__subtitle");
-anime = document.getElementById('animee');
-
-var x = document.getElementsByClassName('stream-info-card__body tw-align-items-start tw-flex tw-flex-column tw-pd-l-0');
-					
-y = document.getElementsByClassName('stream-info-card__body tw-align-items-start tw-flex tw-flex-column tw-pd-l-0');
-  var TextInsideLi = document.getElementsByTagName('p')[1];
-//alert("Wybierz odcinek i kliknij Maki w prawym górnym rogu");
-
 
 function podmiana(odcinek){
 document.getElementById("animo").src= lista[odcinek];
